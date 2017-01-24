20da679a88528bdbf0446036ef7b4c3b73b1736b
Jon Wiedmann <jonwiedmann@gmail.com>
2017-01-24 03:06:19 +0000

    Use iron-ajax

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ server.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
 });
 
-server.get('/dashboard', (req, res) => {
-	res.sendFile(__dirname + '/views/dashboard.html');
-});
-
 server.get('/note', (req, res) => {
 	res.sendFile(__dirname + '/views/note.html');
 });
@@ -22,36 +18,39 @@ server.get('/notes', (req, res) => {
 	res.sendFile(__dirname + '/views/notes.html');
 });
 
-server.get('/pages', (req, res) => {
-	res.sendFile(__dirname + '/views/pages.html');
-});
-
-
 server.get('/notes.json', (reg, res) => {
-	res.json([{
-		id: 'n0',
-		title: 'Have you visited your primary care physician?'
-	},
-	{
-		id: 'n1',
-		title: 'Have you taken your medication this morning?'
-	},
-	{
-		id: 'n2',
-		title: 'How many drinks have you had in the last week?',
-		answerPositive: '0-3',
-		answerNegative: '3+'
-	}]);
+	res.json([
+		{
+			id: 'n0',
+			title: 'Have you visited your primary care physician?'
+		},
+		{
+			id: 'n1',
+			title: 'Have you taken your medication this morning?'
+		},
+		{
+			id: 'n2',
+			title: 'How many drinks have you had in the last week?',
+			answerPositive: '0-3',
+			answerNegative: '3+'
+		}
+	]);
 });
 
-server.get('/car', (reg, res) => {
-	res.json({
-		hp: 305,
-		isSportModel: true
-	});
-});
+// server.get('/dashboard', (req, res) => {
+// 	res.sendFile(__dirname + '/views/dashboard.html');
+// });
 
+// server.get('/pages', (req, res) => {
+// 	res.sendFile(__dirname + '/views/pages.html');
+// });
 
+// server.get('/car', (reg, res) => {
+// 	res.json({
+// 		hp: 305,
+// 		isSportModel: true
+// 	});
+// });
 
 const port = process.env.PORT || 3000;
 console.log(`using port ${port}`);
