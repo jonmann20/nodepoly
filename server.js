759363bc1be7b0d6601e2c69d23ebd8162f1be90
Jon Wiedmann <jonwiedmann@gmail.com>
2017-05-04 18:31:36 +0000

    Before live demo

--- server.js
+++ server.js
@@ -5,59 +5,9 @@ const server = express();
 
 server.use(express.static(__dirname + '/'));
 
-
 server.get('/', (req, res) => {
-	res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
-
-server.get('/note', (req, res) => {
-	res.sendFile(__dirname + '/views/note.html');
-});
-
-server.get('/notes', (req, res) => {
-	res.sendFile(__dirname + '/views/notes.html');
-});
-
-server.get('/notes.json', (reg, res) => {
-	res.json([
-		{
-			id: 'n0',
-			title: 'Have you visited your primary care physician?'
-		},
-		{
-			id: 'n1',
-			title: 'Have you taken your medication this morning?'
-		},
-		{
-			id: 'n2',
-			title: 'How many drinks have you had in the last week?',
-			answerPositive: '0-3',
-			answerNegative: '3+'
-		}
-	]);
-});
-
-server.post('/note-response.json', (reg, res) => {
-	res.json({
-		status: 200,
-		message: 'Saved note response.'
-	})
-});
-
-// server.get('/dashboard', (req, res) => {
-// 	res.sendFile(__dirname + '/views/dashboard.html');
-// });
-
-// server.get('/pages', (req, res) => {
-// 	res.sendFile(__dirname + '/views/pages.html');
-// });
-
-// server.get('/car', (reg, res) => {
-// 	res.json({
-// 		hp: 305,
-// 		isSportModel: true
-// 	});
-// });
 
 const port = process.env.PORT || 3000;
 console.log(`using port ${port}`);
