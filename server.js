ac3db6609818c4dcb252d81a37381b60497a69f9
Jon Wiedmann <jonwiedmann@gmail.com>
2017-07-21 19:44:24 +0000

    Users demo

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const users = [
 		admitted_at: 'Jan 7, 2017 at 8:15am',
 		surveyed_at: 'Jan 7, 2017 at 4:15am',
 		mrn: '423489070',
-		risk_score: 'High'
+		risk_score: 3
 	},
 	{
 		name: 'Joe Kerry',
@@ -22,7 +22,7 @@ const users = [
 		admitted_at: 'Jan 7, 2017 at 9:15am',
 		surveyed_at: 'Jan 7, 2017 at 1:15am',
 		mrn: '13489370',
-		risk_score: 'Medium'
+		risk_score: 2
 	},
 	{
 		name: 'Billy Pharo',
@@ -30,7 +30,7 @@ const users = [
 		admitted_at: 'Jan 7, 2017 at 2:15am',
 		surveyed_at: 'Jan 7, 2017 at 7:15am',
 		mrn: '93488870',
-		risk_score: 'Low'
+		risk_score: 1
 	},
 	{
 		name: 'Aaron Pharo',
@@ -38,7 +38,7 @@ const users = [
 		admitted_at: 'Jan 7, 2017 at 1:15am',
 		surveyed_at: 'Jan 7, 2017 at 2:15am',
 		mrn: '83488870',
-		risk_score: 'Medium'
+		risk_score: 2
 	},
 	{
 		name: 'Carla Pharo',
@@ -46,36 +46,54 @@ const users = [
 		admitted_at: 'Jan 7, 2017 at 2:15am',
 		surveyed_at: 'Jan 7, 2017 at 9:15am',
 		mrn: '73488870',
-		risk_score: 'Low'
+		risk_score: 1
 	}
 ];
 
 server.use(express.static(__dirname + '/'));
 
-const myGraphQLSchema = makeExecutableSchema({
+const schema = makeExecutableSchema({
 	typeDefs: `
+		# A user is used for authentication and identification
 		type User {
 			name: String!
 			client: String!
 			admitted_at: String!
 			surveyed_at: String!
 			mrn: String!
+
+			# The RiskScore will be either Low, Medium, High, or n/a
 			risk_score: String!
+
+			hemoglobin: Int!
+			@deprecated(reason: "Moved to PatientLabResult")
 		}
 
 		type Query {
 			users: [User]
 		}
 	`,
+
 	resolvers: {
 		Query: {
 			users: () => users
+		},
+
+		User: {
+			risk_score(user) {
+				switch(user.risk_score) {
+					case 1: return 'Low';
+					case 2: return 'Medium';
+					case 3: return 'High';
+					default: return 'n/a';
+				}
+			}
 		}
 	}
 });
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
-	schema: myGraphQLSchema
+	schema: schema
 }));
 
 server.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
